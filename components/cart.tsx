01a667e83ae0ace502945136e9b23653f92c7d04
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-03-24 17:38:10 +0100

    add cart usetate

--- components/cart.tsx
+++ components/cart.tsx
@@ -1,22 +1,59 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
 import { useUser } from "@auth0/nextjs-auth0";
-import Layout from "./Layout";
+import { Button } from "react-bootstrap";
 
-const Cart: React.FC = ({ children }) => {
+type CartProps = {
+  cartId: string;
+  removeCart: (createdAt: string) => void;
+};
+
+const Cart: React.FC<CartProps> = ({ cartId, removeCart }) => {
+  //usestate quantity
+  const prix = 10;
+  const [quantity, setQuantity] = React.useState(0);
+  //usestate price
+  const [price, setPrice] = React.useState(prix);
+  const addQuantity = (): void => {
+    setQuantity((number) => number + 1);
+    setPrice((number) => number + number);
+  };
+  const removeQuantity = (): void => {
+    setQuantity((number) => (number !== 0 ? number - 1 : 0));
+    setPrice((number) => number - prix);
+  };
+
   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
+
   return (
     <>
       <tr>
-        <td>Image</td>
-        <td>Product name</td>
-        <td>Price</td>
-        <td>Quantity</td>
-        <td>Actions</td>
-        <td>Total Price</td>
+        <td className="text-center">Image</td>
+        <td className="text-center">Product name</td>
+        <td className="text-center">Prix</td>
+        <td className="text-center">{quantity}</td>
+        <td className="text-center">
+          <Button variant="outline-dark" value="-" onClick={removeQuantity}>
+            -
+          </Button>
+          <span>{quantity}</span>
+          <Button variant="outline-dark" value="+" onClick={addQuantity}>
+            +
+          </Button>
+        </td>
+
+        <td className="text-center">Total Price: {price}</td>
+        <td className="text-center">
+          <Button
+            variant="outline-dark"
+            onClick={(): void => removeCart(cartId)}
+          >
+            Delete
+          </Button>
+        </td>
       </tr>
     </>
   );
--- pages/cartList/index.tsx
+++ pages/cartList/index.tsx
@@ -3,14 +3,30 @@ import Layout from "../../components/Layout";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import Cart from "../../components/cart";
 import { Table } from "react-bootstrap";
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const CartList: React.FC = () => {
+  const cart: string[] = [];
+  const [addCart, setAddCart] = React.useState<string[]>(cart);
+  const reset = (createdAt: string): void => {
+    return setAddCart(addCart.filter((element) => element !== createdAt));
+  };
+  const goCart = (): void => {
+    const idTimer = uuidv4();
+    setAddCart(() => {
+      return [...addCart, idTimer];
+    });
+  };
   return (
     <div className={styles.container}>
       <Layout>
         <main className={styles.main} style={{ padding: "2rem" }}>
           <h1 className={styles.title}>Mon panier</h1>
           <p className={styles.grid}> Listes</p>
+          <button className="btn btn-secondary" onClick={goCart}>
+            Add a game
+          </button>
           <Table striped bordered hover size="sm" style={{ padding: "3rem" }}>
             <thead>
               <tr>
@@ -23,7 +39,11 @@ const CartList: React.FC = () => {
               </tr>
             </thead>
             <tbody>
-              <Cart />
+              {addCart.map((cartList) => {
+                return (
+                  <Cart key={cartList} cartId={cartList} removeCart={reset} />
+                );
+              })}
             </tbody>
           </Table>
         </main>
